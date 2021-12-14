ba448f1f624600835a10add227257ef5d8142daf
jaimemin <jaimemin@gmail.com>
2021-12-14 14:02:32 +0900

    applied ProxyFactory on LogTrace

--- src/main/java/com/tistory/jaimemin/proxy/ProxyApplication.java
+++ src/main/java/com/tistory/jaimemin/proxy/ProxyApplication.java
@@ -6,6 +6,8 @@ import com.tistory.jaimemin.proxy.config.v1_proxy.ConcreteProxyConfig;
 import com.tistory.jaimemin.proxy.config.v1_proxy.InterfaceProxyConfig;
 import com.tistory.jaimemin.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import com.tistory.jaimemin.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import com.tistory.jaimemin.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import com.tistory.jaimemin.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import com.tistory.jaimemin.proxy.trace.logtrace.LogTrace;
 import com.tistory.jaimemin.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +17,9 @@ import org.springframework.context.annotation.Import;
 
 // @Import({AppV1Config.class, AppV2Config.class})
 // @Import(InterfaceProxyConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+// @Import(DynamicProxyFilterConfig.class)
+// @Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "com.tistory.jaimemin.proxy.app") //주의
 public class ProxyApplication {
 
