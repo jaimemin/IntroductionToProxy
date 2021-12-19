e41b548692bde73238a7e0b61e1ae4361f9dd4bd
jaimemin <jaimemin@gmail.com>
2021-12-19 16:54:29 +0900

    applied beanpostprocessor to logtrace
    simplified configuration

--- src/main/java/com/tistory/jaimemin/proxy/ProxyApplication.java
+++ src/main/java/com/tistory/jaimemin/proxy/ProxyApplication.java
@@ -8,6 +8,7 @@ import com.tistory.jaimemin.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig
 import com.tistory.jaimemin.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import com.tistory.jaimemin.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import com.tistory.jaimemin.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import com.tistory.jaimemin.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import com.tistory.jaimemin.proxy.trace.logtrace.LogTrace;
 import com.tistory.jaimemin.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +20,8 @@ import org.springframework.context.annotation.Import;
 // @Import(InterfaceProxyConfig.class)
 // @Import(DynamicProxyFilterConfig.class)
 // @Import(ProxyFactoryConfigV1.class)
-@Import(ProxyFactoryConfigV2.class)
+// @Import(ProxyFactoryConfigV2.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "com.tistory.jaimemin.proxy.app") //주의
 public class ProxyApplication {
 
